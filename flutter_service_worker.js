2c92e3e59cf46de49606770e6febd2de83d13f98
imursv <jspark@bgood.co.kr>
2025-07-10 15:58:51 +0900

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "e44a85dbd98d2fb469e46ebafc3d1c8d",
+const RESOURCES = {"flutter_bootstrap.js": "48bbff3da011da977275a516b9a8d08a",
 "version.json": "961df4da082d265d263784f1689acad2",
-"index.html": "8d9c98065d9dc6920f6fb344d358f330",
-"/": "8d9c98065d9dc6920f6fb344d358f330",
+"index.html": "7970741b3e2e64d678456b89c2e267cc",
+"/": "7970741b3e2e64d678456b89c2e267cc",
 "main.dart.js": "4bbb9b6c01960ea896a323cf2f2b2742",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
